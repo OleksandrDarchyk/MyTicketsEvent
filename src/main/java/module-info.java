b654264422e7ca776b5ef3ec81c97ea7a35999ca
OleksandrDarchyk <oledar01@easv365.dk>
2025-02-05 17:55:46 +0100

    delete module info

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module dk.easv.myticketsevent {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens dk.easv.myticketsevent to javafx.fxml;
-    exports dk.easv.myticketsevent;
-}
