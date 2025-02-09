f8fd398d722a8aecc2c4637c9170164d3fa491a8
OleksandrDarchyk <oledar01@easv365.dk>
2025-02-09 22:47:45 +0100

    CreateEvent.fxml

---
+++ src/main/java/dk/easv/myticketsevent/CreateEventController.java
@@ -0,0 +1,17 @@
+package dk.easv.myticketsevent;
+
+import javafx.event.ActionEvent;
+
+public class CreateEventController {
+    public void ticketsAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteEventAction(ActionEvent actionEvent) {
+    }
+
+    public void cancelAction(ActionEvent actionEvent) {
+    }
+
+    public void saveAction(ActionEvent actionEvent) {
+    }
+}
